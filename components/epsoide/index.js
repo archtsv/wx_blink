c06d9d75e19de4f8e85af2d87b4a5bdd7966e010
cuijianlei <thundersmile@126.com>
2019-05-23 08:40:53 +0800

    new component

--- components/epsoide/index.js
+++ components/epsoide/index.js
@@ -4,15 +4,25 @@ Component({
    * 组件的属性列表
    */
   properties: {
-    index: Number
+    index: {
+      type: Number,
+      observer(newVal, oldVal, changePath) {
+        let val = newVal < 10 ? `0${newVal}` : `${newVal}`
+        this.setData({
+          _index: val
+        })
+      }
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    year: Number,
-    month: String,
+    months: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+    year: 0,
+    month: '',
+    _index: ''
   },
 
   /**
@@ -20,5 +30,19 @@ Component({
    */
   methods: {
 
+  },
+  
+  lifetimes: {
+    attached() {
+      let date = new Date()
+      let year = date.getFullYear()
+      let month = date.getMonth()
+      
+      this.setData({
+        year,
+        month: this.data.months[month]
+      })
+
+    }
   }
 })
