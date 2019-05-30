dc18bb28d441432cd37f2dfa4870c95bfb009a2e
cuijianlei <thundersmile@126.com>
2019-05-30 17:33:26 +0800

    book-detail

--- pages/book-detail/book-detail.js
+++ pages/book-detail/book-detail.js
@@ -1,18 +1,57 @@
 // pages/book-detail/book-detail.js
+import {
+  BookModel
+} from '../../models/book';
+import {
+  LikeModel
+} from '../../models/like';
+const bookModel = new BookModel();
+const likeModel = new LikeModel();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    comments: [],
+    detail: null,
+    likeStatus: 0,
+    likeCount: 0,
+  },
 
+  onLike(event) {
+    const like_or_cancel = event.detail.behavior;
+    likeModel.like(like_or_cancel, this.data.detail.id, 400);
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const bid = options.bid;
+    const detail = bookModel.getDetail(bid);
+    const comments = bookModel.getCommets(bid);
+    const likeStatus = bookModel.getLikeStatus(bid);
+    detail.then(res => {
+      console.log(res)
+      this.setData({
+        detail: res,
+      })
+    });
+    comments.then(res => {
+      console.log(res)
+      this.setData({
+        comments: res.comments,
+      })
+    });
+    likeStatus.then(res => {
+      console.log(res)
+      this.setData({
+        likeStatus: res.like_status,
+        likeCount: res.fav_nums,
+      })
+    });
   },
 
   /**
